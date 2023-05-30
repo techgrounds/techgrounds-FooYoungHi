b09a94cf3ead7aba35df2f36fd15d88acbbd80a9
Danny Stammers <121437669+FooYoungHi@users.noreply.github.com>
2023-05-30 11:18:53 +0200

    Add files via upload

---
+++ 00_includes/PRG-02-1.py
@@ -0,0 +1,6 @@
+x = 6;
+y = 8;
+z = x+y;
+print(x);
+print(y);
+print(z);
---
+++ 00_includes/PRG-02-3.py
@@ -0,0 +1,6 @@
+waarde = "Een waarde";
+print("Value 1:" + waarde);
+waarde = "Iets anders";
+print("Value 2:" + waarde);
+waarde = "Nu stop ik er mee";
+print("Value 3:" + waarde);
