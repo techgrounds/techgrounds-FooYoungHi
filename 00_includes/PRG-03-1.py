8b0015dcd410b86953b72e9c3afefb9a80ba05d7
Danny Stammers <121437669+FooYoungHi@users.noreply.github.com>
2023-05-30 14:19:30 +0200

    Add files via upload

---
+++ 00_includes/PRG-03-1.py
@@ -0,0 +1,15 @@
+#Setting the variables:
+a = 'int';
+b = 7;
+c = False;
+d = 18.5;
+
+#Determining the variable type:
+print("variable a = ", type(a));
+print("variable b = ", type(b));
+print("variable c = ", type(c));
+print("variable d =" , type(d));
+
+#Adding an 'int' and a 'float':
+x = b + d;
+print("Variable x =", x);
---
+++ 00_includes/PRG-03-2.py
@@ -0,0 +1,4 @@
+print("Hoe heet je?")
+x = input();
+print("Hoi, " + x);
+print("De variable was een: ", type(x))
