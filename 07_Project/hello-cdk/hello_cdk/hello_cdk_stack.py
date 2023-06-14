28b6c0f2eb3ebd9cd0d32ca80359cd0ba6813469
Danny Stammers <121437669+FooYoungHi@users.noreply.github.com>
2023-06-14 13:24:53 +0200

    Initial Commit

---
+++ 07_Project/hello-cdk/hello_cdk/hello_cdk_stack.py
@@ -0,0 +1,12 @@
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
+            
+class HelloCdkStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(self, "MyFirstAmazingBucket",
+            versioned=True,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True)
